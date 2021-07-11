4ac598ce23bcdba3791ad18ede7b5984e9230769
Alex Kent <alex.kent1@hotmail.com>
2021-07-11 09:58:43 +0100

    Added explicit conversions for serialisable_enum to/from int

--- main.cpp
+++ main.cpp
@@ -13,19 +13,34 @@ std::vector<serialisable_enum<Fish>::value> serialisable_enum<Fish>::str_values_
 
 struct Fish : public serialisable_enum<Fish>
 {
-  static inline Fish::value Minnow = Fish::value("minnow");
+  static inline Fish::value Minnow = Fish::value("minnow", 0);
   static inline Fish::value Shark = Fish::value("shark");
+  static inline Fish::value Dolphin = Fish::value("dolphin", 10);
+  static inline Fish::value Barracuda = Fish::value("barracuda", 1);
+  static inline Fish::value Tuna = Fish::value("tuna");
 };
+
+std::ostream& operator<<(std::ostream& os, const Fish::value& fish)
+{
+  os << fish.to_string();
+  return os;
+}
 
 int main()
 {
-  std::cout << Fish::Minnow.to_string() << std::endl;
+  std::cout << Fish::Minnow << std::endl;
 
   Fish::value shark = Fish::Shark;
-  std::cout << shark.to_string() << std::endl;
+  std::cout << shark << std::endl;
 
   Fish::value minnow = Fish::from_string("minnow");
-  std::cout << minnow.to_string() << std::endl;
+  std::cout << minnow << std::endl;
+
+  int tuna = static_cast<int>(Fish::Tuna);
+  std::cout << tuna << std::endl;
+
+  Fish::value dolphin = Fish::value(10);
+  std::cout << dolphin << std::endl;
 
   return 0;
 }
--- serialisable_enum.h
+++ serialisable_enum.h
@@ -1,6 +1,9 @@
+#ifndef __SERIALISABLE_ENUM_INCLUDED__
+#define __SERIALISABLE_ENUM_INCLUDED__ 
+
+#include <stdexcept>
 #include <string>
 #include <vector>
-#include <stdexcept>
 
 namespace nstd
 {
@@ -13,37 +16,87 @@ public:
   {
   public:
     value() = delete;
+    value(std::string&&);
+    value(std::string&&, int);
 
-    value(std::string&& repr) : repr_(repr)
-    {
-      serialisable_enum<EnumType>::str_values_.push_back(*this);
-    }
+    // Explicit conversions.
+    explicit value(int);
+    inline explicit operator int() const { return id_; }
 
-    std::string to_string() const
-    {
-      return repr_;
-    }
+    inline std::string to_string() const { return repr_; }
 
   private:
+    using enum_type = serialisable_enum<EnumType>;
+    int id_ = 0;
     std::string repr_;
   };
 
-  static serialisable_enum<EnumType>::value from_string(
-    const std::string& repr)
-  {
-    for (const auto& val : serialisable_enum<EnumType>::str_values_)
-    {
-      if (val.to_string() == repr)
-      {
-        return val;
-      }
-    }
-
-    throw std::exception();
-  }
+  static serialisable_enum<EnumType>::value from_string(const std::string&);
 
 private:
   static std::vector<serialisable_enum<EnumType>::value> str_values_;
 };
 
+template <typename EnumType>
+serialisable_enum<EnumType>::value::value(
+  std::string&& repr)
+:
+  repr_(repr)
+{
+  // Infer the id of this element by looking at the latest element.
+  if (!enum_type::str_values_.empty())
+  {
+    id_ = enum_type::str_values_.back().id_ + 1;
+  }
+
+  enum_type::str_values_.push_back(*this);
 }
+
+template <typename EnumType>
+serialisable_enum<EnumType>::value::value(
+  std::string&& repr, int id)
+:
+  repr_(repr), id_(id)
+{
+  enum_type::str_values_.push_back(*this);
+}
+
+template <typename EnumType>
+serialisable_enum<EnumType>::value::value(
+  int id)
+:
+  repr_(std::string{}), id_(id)
+{
+  for (const auto& val : enum_type::str_values_)
+  {
+    if (val.id_ == id)
+    {
+      repr_ = val.repr_;
+    }
+  }
+
+  if (repr_ == std::string{})
+  {
+    throw std::exception();
+  }
+}
+
+template <typename EnumType>
+typename serialisable_enum<EnumType>::value
+  serialisable_enum<EnumType>::from_string(
+    const std::string& repr)
+{
+  for (const auto& val : serialisable_enum<EnumType>::str_values_)
+  {
+    if (val.to_string() == repr)
+    {
+      return val;
+    }
+  }
+
+  throw std::exception();
+}
+
+} // namespace nstd
+
+#endif // __SERIALISABLE_ENUM_INCLUDED__
